cc9d5b08860c4be31ff8016634948968128bddf7
Imheroman <duddnd4457@naver.com>
2023-10-24 22:36:35 +0900

    nhn mart commit for studying Thread

---
+++ src/main/java/threadMart/Consumer.java
@@ -0,0 +1,53 @@
+package threadMart;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/*
+소비자는 매장에 입장 후 물건을 구매할 수 있다.
+매장에는 입장 인원 제한이 있으므로, 인원 초과시 기다린다.
+매장에 입장하면 물건을 구매하고, 퇴장한다.
+1~10초 간격으로 구매한다.
+ */
+
+public class Consumer implements Runnable {
+    private String name;
+    private Store store;
+    private int count = 0;
+    private final int END_NUMBERS = 3;
+    public final static int INTERVAL_TIMES = 1000;
+    private int interval;
+
+    public Consumer(String name, Store store) {
+        this.name = name;
+        this.store = store;
+    }
+
+    private void itemBuying() {
+        store.sell();
+    }
+
+    public void stop() {
+        Thread.currentThread().interrupt();
+    }
+
+    @Override
+    public void run() {
+        while (count < END_NUMBERS) { // 물품 구매 횟수
+            interval = ThreadLocalRandom.current().nextInt(INTERVAL_TIMES, INTERVAL_TIMES * 10);
+            itemBuying();
+            count++;
+
+            try {
+                Thread.sleep(interval);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        store.exit(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/threadMart/Producer.java
@@ -0,0 +1,57 @@
+package threadMart;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+/*
+생산자는 매장에 물건이 부족하지 않도록 채움
+물건은 1~10초 간격으로 채움
+Thread내에서 난수 생성을 위해서는 ThreadLocalRandom.current().nextInt()를 사용
+ */
+
+public class Producer implements Runnable {
+    private List<Item> items;
+    private Store store;
+    private int interval = 0;
+    public static final int INTERVAL_TIMES = 1000;
+    public static final int ITEMS_SIZE = 100;
+
+    public Producer(Store store) {
+        this.store = store;
+        this.items = new ArrayList<>(ITEMS_SIZE);
+    }
+
+    private void produce() {
+        Item item = new Item();
+        items.add(item);
+    }
+
+    private void sellItem() {
+        if (store.getItems().size() <= 10) {
+            Item item = this.items.get(0);
+            store.buy(item);
+            this.items.remove(0);
+        }
+    }
+
+    public void stop() {
+        Thread.currentThread().interrupt();
+    }
+
+    @Override
+    public void run() {
+        int size = 0;
+        while (ITEMS_SIZE > size) {
+            interval = ThreadLocalRandom.current().nextInt(INTERVAL_TIMES, INTERVAL_TIMES * 10);
+            try {
+                this.produce();
+                this.sellItem();
+                Thread.sleep(interval);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            size++;
+        }
+    }
+}
